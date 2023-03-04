0f35e57be73f80f889c17bb55c28d8056497f905
CranFused <121121675+CranFused-Real@users.noreply.github.com>
2023-03-04 22:44:03 +0530

    Add files via upload
    A simple javascript payload which can root account's file without actually logging as root..

---
+++ root_reader.js
@@ -0,0 +1,7 @@
+/* This code can read the system/root level files without actually logging as root or using sudo if 'sudo su' or sudo command is blocked...
+*/
+
+const fs = require('fs');
+
+fs.readFile('<directory here>', 'utf8', (err, data) => { if (err) throw err; console.log(data); Agent 102-0) Deck/2000 Preter/102.6
+ept-Language; en US enged.
