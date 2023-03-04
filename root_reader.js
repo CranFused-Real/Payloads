039867fe6d3c70a88dd27c32a134000687da7ebc
CranFused <121121675+CranFused-Real@users.noreply.github.com>
2023-03-04 22:48:30 +0530

    Update root_reader.js

--- root_reader.js
+++ root_reader.js
@@ -3,7 +3,7 @@
 
 const fs = require('fs');
 
-fs.readFile('/root/root.txt', 'utf8', (err, data) => { 
+fs.readFile('<directory here>', 'utf8', (err, data) => { 
   if (err) throw err; 
   console.log(data); 
 });
