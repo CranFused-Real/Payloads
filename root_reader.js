7362d81d4b55d853b5a8b11ae9a017841c41fc84
CranFused <121121675+CranFused-Real@users.noreply.github.com>
2023-03-04 22:47:25 +0530

    Update root_reader.js

--- root_reader.js
+++ root_reader.js
@@ -3,5 +3,7 @@
 
 const fs = require('fs');
 
-fs.readFile('<directory here>', 'utf8', (err, data) => { if (err) throw err; console.log(data); Agent 102-0) Deck/2000 Preter/102.6
-ept-Language; en US enged.
+fs.readFile('/root/root.txt', 'utf8', (err, data) => { 
+  if (err) throw err; 
+  console.log(data); 
+});
